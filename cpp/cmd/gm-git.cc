78b040d3ef62a61121d8b7b5085bbf61153ec5a3
gavin <gavin.w.mead@gmail.com>
2024-05-18 18:37:13 -0400

    initial setup

---
+++ cpp/cmd/gm-git.cc
@@ -0,0 +1,12 @@
+//
+// Created by Gavin Mead on 5/18/24.
+//
+
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "Hello world!" << std::endl;
+
+    return 0;
+}
---
+++ cpp/lib/add.cc
@@ -0,0 +1,9 @@
+//
+// Created by Gavin Mead on 5/18/24.
+//
+
+#include "my_math.h"
+
+int add(int x, int y) {
+    return x + y;
+}
