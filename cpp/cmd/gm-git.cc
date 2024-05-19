8a722748f6979059eebc0c909faeedf7d1a1bf5b
Gavin Mead <gavin.w.mead@gmail.com>
2024-05-19 11:05:53 -0400

    adds abseil lib (#3)
    * started some design docs...nothing exciting

--- cpp/cmd/gm-git.cc
+++ cpp/cmd/gm-git.cc
@@ -4,9 +4,15 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+
+#include "absl/strings/str_join.h"
 
 int main() {
-    std::cout << "Hello world!" << std::endl;
+    std::vector<std::string> v = {"foo", "bar", "baz"};
+    std::string s = absl::StrJoin(v, "-");
+
+    std::cout << "Joined string: " << s << "\n";
 
     return 0;
 }
