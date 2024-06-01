7373348d8b9705e142152948347b86efb2b7914f
gavin <gavin.w.mead@gmail.com>
2024-06-01 13:10:57 -0400

    c++17 support

--- cpp/cmd/gm-git.cc
+++ cpp/cmd/gm-git.cc
@@ -5,8 +5,15 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <optional>
 #include "absl/strings/str_join.h"
+
+std::optional<int> divide(int num1, int num2) {
+    if (num2 != 0) {
+        return num1/num2;
+    }
+    return std::nullopt; // Indicates no type-safe value
+}
 
 int main(int argc, const char* argv[]) {
 
@@ -19,6 +26,13 @@ int main(int argc, const char* argv[]) {
     std::string s = absl::StrJoin(v, "-");
 
     std::cout << "Joined string: " << s << "\n";
+
+    auto result = divide(10, 2); // To infer into std::optional<int>, used to save time
+    if (result.has_value()) {  // has_value checks if a type-value is returned
+        std::cout << "Result: " << result.value() << std::endl; // value returns the value as function arguments are correct
+    } else {
+        std::cout << "Division by zero" << std::endl;
+    }
 
     return 0;
 }
